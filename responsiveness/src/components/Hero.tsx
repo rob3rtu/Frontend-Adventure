16e42c0b9c071064b4ae5fb0e24afed7512b3223
Robert <robert.udrea@cst.ro>
2024-11-22 20:24:35 +0200

    what am i doing

---
+++ responsiveness/src/components/Hero.tsx
@@ -0,0 +1,17 @@
+import Section from "./Section";
+
+const Hero = () => {
+  return (
+    <Section
+      className={""}
+      id={""}
+      crosses={false}
+      crossesOffset={false}
+      customPadding={""}
+    >
+      <div></div>
+    </Section>
+  );
+};
+
+export default Hero;
